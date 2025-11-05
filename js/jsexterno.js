5fdbc96e328cfb405e938496369cb71bd4c498ab
mauro <mauroalejo2000@hotmail.com>
2025-11-05 18:20:42 -0500

    CalculadoraDeber

--- js/jsexterno.js
+++ js/jsexterno.js
@@ -25,13 +25,13 @@ function evaluarOperacion(tipo){
         resultado = sumar(num1, num2);
     }
     if(tipo==='-'){
-        
+        resultado = restar(num1, num2);
     }
     if(tipo==='*'){
-        
+        resultado = multiplicar(num1, num2);
     }
     if(tipo==='/'){
-        
+        resultado = dividir(num1, num2);
     }
     document.getElementById('id_resultado').innerText = resultado;
 }
