9008fb90e5fe539409974116b04c97648f56fb8d
mauro <mauroalejo2000@hotmail.com>
2025-11-07 19:42:06 -0500

    Taller8

---
+++ js/calculadoraFinal.js
@@ -0,0 +1,27 @@
+let num1 = 0;
+let num2 = 0;
+
+function mostrarDisplay(valor) {
+    let elemento = document.getElementById("display");
+    elemento.innerText = elemento.innerText + valor;
+}
+
+function sumar() {
+
+}
+
+function restar() {
+
+}
+
+function multiplicar() {
+
+}
+
+function dividir() {
+
+}
+
+function porcentaje() {
+
+}
